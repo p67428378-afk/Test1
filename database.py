a0c064180aa94ec73cd168041aecebc62b4db49b
p67428378-afk <p67428378@gmail.com>
2026-03-26 06:07:24 +0000

    Add comment about bug in set_password method
    Comment added to indicate a bug introduction in set_password method.

--- database.py
+++ database.py
@@ -18,6 +18,6 @@ class User(db.Model):
 
     def set_password(self, password):
         self.password_hash = bcrypt.generate_password_hash(password).decode('utf-8')
-
+    #bug introduced here
     def check_password(self, password):
         return bcrypt.check_password_hash(self.password_hash, password)
